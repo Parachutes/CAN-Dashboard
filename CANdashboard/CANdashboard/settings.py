ceb5469a96113e8b34a47453c0b7866eea347504
mashichao <hzmashichao@163.com>
2018-02-12 16:15:46 +0000

    delete the dashboard file

--- CANdashboard/CANdashboard/settings.py
+++ CANdashboard/CANdashboard/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Dashboard',
     'forms_builder.forms',
     'django.contrib.sites',
     # $ install the app {Shichao Ma / 12 Feb}
