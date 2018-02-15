490295209041460f8f8a600313022537e8bb82aa
Omar Nassef <omar.nassef@kcl.ac.uk>
2018-02-15 13:16:17 +0000

    improved the initial design for prototype

--- CANdashboard/dashboard/views.py
+++ CANdashboard/dashboard/views.py
@@ -9,16 +9,6 @@ def index(request):
     template = loader.get_template('app/index.html')
     return HttpResponse(template.render(context, request))
 
-def indexUser(request):
-    context = {}
-    template = loader.get_template('app/indexUser.html')
-    return HttpResponse(template.render(context, request))
-
-def indexAdmin(request):
-    context = {}
-    template = loader.get_template('app/indexAdmin.html')
-    return HttpResponse(template.render(context, request))
-
 
 def gentella_html(request):
     context = {}
@@ -32,5 +22,5 @@ def gentella_html(request):
 
 def Charity_detail(request,Name):
         charity = Charity.objects.filter(slug=Name)
-        template = loader.get_template('dashboard/index.html')
-        return render(request,'dashboard/index.html',{'charity': charity})
+        template = loader.get_template('app/index.html')
+        return render(request,'app/index.html',{'charity': charity})
