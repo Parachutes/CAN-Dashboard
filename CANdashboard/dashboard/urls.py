5b7ba672b981e427be7c1e3133eaba3f5c03810a
Omar Nassef <omar.nassef@kcl.ac.uk>
2018-02-23 19:13:18 +0000

    add login structure and proper redirect of page when succesfully logs in

--- CANdashboard/dashboard/urls.py
+++ CANdashboard/dashboard/urls.py
@@ -3,22 +3,28 @@ from dashboard import views
 from django.contrib import admin
 import forms_builder.forms.urls
 
+
 urlpatterns = [
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
     # Or create a separate django app.
-    url(r'^.*\.html', views.gentella_html, name='gentella'),
-
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url('accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+                          {'next_page': '/'}),
+    url(r'^messages/', views.list_messages),             
     url(r'^forms/', include(forms_builder.forms.urls)),
+    url(r'^profile/$', views.indexUser, name='indexUser'),
 
     # pages for audiences/ users/ administrators
-    url(r'^$', views.index, name='index'),
-    url(r'^myUser/', views.indexUser, name='indexUser'),
-    url(r'^myAdmin/', views.indexAdmin, name='indexAdmin'),
+
+    #url(r'^myUser/', views.indexUser, name='indexUser'),
+    #url(r'^myAdmin/', views.indexAdmin, name='indexAdmin'),
 
     # to be deleted just for testing
-    url(r'^test/', views.indexTest, name='indexTest'),
+    #url(r'^test/', views.indexTest, name='indexTest'),
 
     url(r'(?P<Name>[-\w]+)/$',views.Charity_detail, name = 'charity_detail'),
 
--- CANdashboard/dashboard/views.py
+++ CANdashboard/dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Charity, User
+from django.contrib.auth.decorators import login_required
+from directmessages.apps import Inbox
 
 
 
@@ -14,42 +16,31 @@ def indexUser(request):
     context = {}
     template = loader.get_template('app/indexUser.html')
     return HttpResponse(template.render(context, request))
+#
+# def indexAdmin(request):
+#     context = {}
+#     template = loader.get_template('app/indexAdmin.html')
+#     return HttpResponse(template.render(context, request))
 
-def indexAdmin(request):
-    context = {}
-    template = loader.get_template('app/indexAdmin.html')
-    return HttpResponse(template.render(context, request))
-
-def indexTest(request):
-    context = {}
-    template = loader.get_template('app/indexBackUp.html')
-    return HttpResponse(template.render(context, request))
+# def indexTest(request):
+#     context = {}
+#     template = loader.get_template('app/indexBackUp.html')
+#     return HttpResponse(template.render(context, request))
 
 
-def gentella_html(request):
-    context = {}
-    # The template to be loaded as per gentelella.
-    # All resource paths for gentelella end in .html.
-
-    # Pick out the html file name from the url. And load that template.
-    load_template = request.path.split('/')[-1]
-    template = loader.get_template('app/' + load_template)
-    return HttpResponse(template.render(context, request))
 
 def Charity_detail(request,Name):
         charity = Charity.objects.filter(slug=Name)
         template = loader.get_template('app/index.html')
         return render(request,'app/index.html',{'charity': charity})
 
-        
 
-#TODO request username for list messages from login
-#def list_messages(request):
-#    touser =User.objects.get(username ='ALS')
-#    touser.username = touser
-#    mes = Inbox.get_unread_messages(touser).values_list('content',flat=True)
-#    html = "<html><body>It is now %s.</body></html>" % mes
-#    return HttpResponse(html)
+@login_required
+def list_messages(request):
+    user = User.objects.get(username=request.user.username)
+    mes = Inbox.get_unread_messages(user).values_list('content',flat=True)
+    html = "<html><body>It is now %s.</body></html>" % mes
+    return HttpResponse(html)
 
 # TODO request other charity name and obtain from_user from login
 #def send_message(request):
