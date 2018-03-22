818c04a7358e8b2afc341223dde27bd147365ffa
Omar Nassef <omar.nassef@kcl.ac.uk>
2018-03-22 20:13:02 +0000

    add the ability to integrate reuslts into marks to pick out the result match the mark

--- CANdashboard/dashboard/urls.py
+++ CANdashboard/dashboard/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     url(r'^Financial/$', views.FinancialCategory, name='finance'),
     url(r'^Strength/$', views.StrengthCategory, name='Strength'),
     url(r'^Progress/$', views.ProgressCategory, name='progress'),
-    #url(r'^bla/(?P<slug>[-\w\d]+)/$', views.FormDetail.as_view(), name='getsurvey'),
+    url(r'^bla/$', views.CalculateMarking, name='gen'),
     url(r'^Analysis/(?P<id>[-\w\d]+)/$',views.surveyAnalysis, name = 'analysis_survey'),
     url(r'^deleteSurvey/(?P<id>[-\w\d]+)/$', views.deleteSurvey, name='deleteSurvey'),
 
