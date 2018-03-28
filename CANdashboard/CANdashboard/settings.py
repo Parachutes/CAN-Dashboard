dc7685cef0a134ec80c454123884bf24ff78cd37
Omar Nassef <omar.nassef@kcl.ac.uk>
2018-03-28 21:05:31 +0100

    revert to changes pre heroku app

--- CANdashboard/CANdashboard/settings.py
+++ CANdashboard/CANdashboard/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = (
     'dashboard',
     'directmessages',
     'crispy_forms',
-    'django_heroku'
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
@@ -120,14 +119,6 @@ LOGIN_REDIRECT_URL = '/profile/'
 AUTH_USER_MODEL = 'auth.User'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
 
 
 # Static files (CSS, JavaScript, Images)
